3d393552088944b0ee48c18010c30c5dd33e4fb2
ElKratos259 <elkratosxdd@outlook.com>
2025-07-11 21:05:56 -0500

    creation of entity controllers

---
+++ controller/ProyectoController.php
@@ -0,0 +1,49 @@
+<?php
+    require_once __DIR__ . '/../model/ProyectoModel.php';
+    require_once __DIR__ . '/../model/ClienteModel.php'
+
+    class ProyectoController {
+        public function cargar()
+        {
+            $model = new ProyectoModel();
+            $proyectos = $model->cargar();
+            require_once __DIR__ . '/../view/viewCargarProyectos.php';
+        }
+
+        public function guardar()
+        {
+            $model = new ClienteModel();
+            $clientes = $model->cargar();
+            if ($_SERVER['REQUEST_METHOD']=='POST') {
+                $model = new ProyectoModel();
+                $proyecto = new Proyecto();
+                $proyecto->setNombre($_POST['txtNom']);
+                $proyecto->setDescripcion($_POST['txtDes']);
+                $proyecto->setEstado($_POST['txtEst']);
+                $proyecto->setIdcliente($_POST['txtTel']);
+                $model->guardar($proyecto);
+                header('Location: index.php');
+            } else {
+                require_once __DIR__ . '/../view/view/viewGuardarProyecto.php';
+            }
+        }
+
+        public function modificar() 
+        {
+            $model = new ClienteModel();
+            $clientes = $model->cargar();
+            if ($_SERVER['REQUEST_METHOD']=='POST') {
+                $model = new ProyectoModel();
+                $proyecto = new Proyecto();
+                $proyecto->setNombre($_POST['txtNom']);
+                $proyecto->setDescripcion($_POST['txtDes']);
+                $proyecto->setEstado($_POST['txtEst']);
+                $proyecto->setIdcliente($_POST['txtTel']);
+                $model->modificar($proyecto);
+                header('Location: index.php');
+            } else {
+                require_once __DIR__ . '/../view/viewModificarProyecto.php';
+            }
+        }
+    }
+?>
---
+++ controller/UsuarioController.php
@@ -0,0 +1,53 @@
+<?php
+    require_once __DIR__ . '/../model/UsuarioModel.php';
+    require_once __DIR__ . '/../model/Usuario.php'; 
+
+    class UsuarioController {
+        public function validar() 
+        {
+            session_start();
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $email = $_POST['email'] ?? '';
+                $password = $_POST['password'] ?? '';
+
+                $usuario = new Usuario(null, null, $email, $password, null);
+
+                $model = new UsuarioModel();
+                $datos = $model->validar($usuario);
+
+                if ($datos) {
+                    $_SESSION['idUsuario'] = $datos['idUsuario'];
+                    $_SESSION['nombre'] = $datos['nomUsuario'];
+                    $_SESSION['rol'] = $datos['rol'];
+
+                    header("Location: dashboard.php");
+                    exit();
+                } else {
+                    $_SESSION['error_login'] = "Correo o contraseña incorrectos.";
+                    header("Location: index.php");
+                    exit();
+                }
+            } else {
+                header("Location: index.php");
+                exit();
+            }
+        }
+
+        public function guardar() 
+        {
+            if ($_SERVER['REQUEST_METHOD']=='POST') {
+                $model = new UsuarioModel();
+                $usuario = new Usuario();
+                $usuario->setNomUsuario($_POST['txtNom']);
+                $usuario->setEmail($_POST['txtDes']);
+                $usuario->setPassword($_POST['txtPass']);
+                $usuario->setRol($_POST['txtRol']);
+                $model->guardar($usuario);
+                header('Location: index.php');
+            } else {
+                require_once __DIR__ . '/../view/viewGuardarFamilia.php';
+            }
+        }
+    }
+?>
